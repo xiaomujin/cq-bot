8cd128d0c16746e49e62d957b6d458c1849fff21
KuroNeko <1419229777@qq.com>
2023-07-06 10:41:11 +0800

    update: icqq适配暂不使用

--- src/main/java/com/kuroneko/cqbot/shiro/MyActionHandler.java
+++ src/main/java/com/kuroneko/cqbot/shiro/MyActionHandler.java
@@ -1,25 +1,25 @@
-package com.kuroneko.cqbot.shiro;
-
-import com.alibaba.fastjson2.JSONObject;
-import com.mikuac.shiro.enums.ActionPath;
-import com.mikuac.shiro.handler.ActionHandler;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.annotation.Primary;
-import org.springframework.stereotype.Component;
-import org.springframework.web.socket.WebSocketSession;
-
-import java.util.Map;
-
-@Slf4j
-@Primary
-@Component
-public class MyActionHandler extends ActionHandler {
-    @Override
-    public JSONObject action(WebSocketSession session, ActionPath action, Map<String, Object> params) {
-        JSONObject jsonObject = super.action(session, action, params);
-        if (jsonObject != null && jsonObject.getJSONObject("data") != null && jsonObject.getJSONObject("data").containsKey("message_id")) {
-            jsonObject.getJSONObject("data").put("message_id", 1);
-        }
-        return jsonObject;
-    }
-}
+//package com.kuroneko.cqbot.shiro;
+//
+//import com.alibaba.fastjson2.JSONObject;
+//import com.mikuac.shiro.enums.ActionPath;
+//import com.mikuac.shiro.handler.ActionHandler;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.context.annotation.Primary;
+//import org.springframework.stereotype.Component;
+//import org.springframework.web.socket.WebSocketSession;
+//
+//import java.util.Map;
+//
+//@Slf4j
+//@Primary
+//@Component
+//public class MyActionHandler extends ActionHandler {
+//    @Override
+//    public JSONObject action(WebSocketSession session, ActionPath action, Map<String, Object> params) {
+//        JSONObject jsonObject = super.action(session, action, params);
+//        if (jsonObject != null && jsonObject.getJSONObject("data") != null && jsonObject.getJSONObject("data").containsKey("message_id")) {
+//            jsonObject.getJSONObject("data").put("message_id", 1);
+//        }
+//        return jsonObject;
+//    }
+//}
--- src/main/java/com/kuroneko/cqbot/shiro/MyMessageEvent.java
+++ src/main/java/com/kuroneko/cqbot/shiro/MyMessageEvent.java
@@ -1,20 +1,20 @@
-package com.kuroneko.cqbot.shiro;
-
-import com.alibaba.fastjson2.JSONObject;
-import com.mikuac.shiro.core.Bot;
-import com.mikuac.shiro.handler.event.MessageEvent;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.annotation.Primary;
-import org.springframework.stereotype.Component;
-
-@Slf4j
-@Primary
-@Component
-public class MyMessageEvent extends MessageEvent {
-    @Override
-    public void handler(Bot bot, JSONObject resp) {
-        resp.put("message_id", 1);
-        resp.put("font", 1);
-        super.handler(bot, resp);
-    }
-}
+//package com.kuroneko.cqbot.shiro;
+//
+//import com.alibaba.fastjson2.JSONObject;
+//import com.mikuac.shiro.core.Bot;
+//import com.mikuac.shiro.handler.event.MessageEvent;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.context.annotation.Primary;
+//import org.springframework.stereotype.Component;
+//
+//@Slf4j
+//@Primary
+//@Component
+//public class MyMessageEvent extends MessageEvent {
+//    @Override
+//    public void handler(Bot bot, JSONObject resp) {
+//        resp.put("message_id", 1);
+//        resp.put("font", 1);
+//        super.handler(bot, resp);
+//    }
+//}
