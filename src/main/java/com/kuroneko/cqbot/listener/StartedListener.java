2c069ee5d081216e9c4928226e23a22da6d07a7b
KuroNeko <1419229777@qq.com>
2023-07-27 17:45:55 +0800

    update: 初始化浏览器

--- src/main/java/com/kuroneko/cqbot/listener/StartedListener.java
+++ src/main/java/com/kuroneko/cqbot/listener/StartedListener.java
@@ -2,6 +2,7 @@ package com.kuroneko.cqbot.listener;
 
 import com.kuroneko.cqbot.service.BotTaskService;
 import com.kuroneko.cqbot.service.TarKovMarketService;
+import com.kuroneko.cqbot.utils.PuppeteerUtil;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.context.event.ApplicationStartedEvent;
@@ -18,9 +19,10 @@ public class StartedListener implements ApplicationListener<ApplicationStartedEv
     @Override
     public void onApplicationEvent(ApplicationStartedEvent event) {
         log.info("系统启动成功");
+        PuppeteerUtil.getBrowser();
         botTaskService.refreshDaily();
         botTaskService.refreshRiLi();
-        botTaskService.refreshAgeListVo();
+//        botTaskService.refreshAgeListVo();
         botTaskService.refreshThreeDog();
         botTaskService.refreshTKFDT();
         tarKovMarketService.cacheTkfServerInfo();
