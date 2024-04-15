e1756d89edef518a228a7110608e863aaa91069c
KuroNeko <1419229777@qq.com>
2024-04-15 17:54:47 +0800

    update: 新跳蚤查询

--- src/main/java/com/kuroneko/cqbot/annoPlugin/TkfServerPlugin.java
+++ src/main/java/com/kuroneko/cqbot/annoPlugin/TkfServerPlugin.java
@@ -1,5 +1,6 @@
 package com.kuroneko.cqbot.annoPlugin;
 
+import cn.hutool.core.date.LocalDateTimeUtil;
 import cn.hutool.core.util.StrUtil;
 import com.alibaba.fastjson2.JSON;
 import com.alibaba.fastjson2.JSONArray;
@@ -32,6 +33,9 @@ import org.springframework.stereotype.Component;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
+import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.*;
 import java.util.concurrent.TimeUnit;
 import java.util.regex.Matcher;
@@ -99,16 +103,7 @@ public class TkfServerPlugin {
             }
             log.info("开始更新任务数据");
             bot.sendMsg(event, "开始更新任务数据", false);
-            HashMap<String, String> queryMap = getQueryMap();
-            String url = "https://api.tarkov.dev/graphql";
-            HttpClient httpClient = HttpUtil.getHttpClient();
-            HttpRequest httpRequest = HttpRequest.newBuilder()
-                    .uri(URI.create(url))
-                    .header("Content-Type", "application/json")
-                    .header("Accept", "application/json")
-                    .POST(HttpRequest.BodyPublishers.ofString(JSON.toJSONString(queryMap)))
-                    .build();
-            String request = HttpUtil.request(httpClient, url, httpRequest);
+            String request = getQueryRes(QUERY_TASKS);
             JSONArray jsonArray = JSON.parseObject(request).getJSONObject("data").getJSONArray("tasks");
             if (jsonArray.isEmpty()) {
                 return "更新失败";
@@ -165,66 +160,180 @@ public class TkfServerPlugin {
 
     }
 
-    private static HashMap<String, String> getQueryMap() {
-        String query = """
-                {
-                  tasks(lang: zh) {
-                    id
-                    name
-                    trader {
-                      name
-                      imageLink
+    private static final String QUERY_TASKS = """
+            {
+              tasks(lang: zh) {
+                id
+                name
+                trader {
+                  name
+                  imageLink
+                }
+                taskImageLink
+                minPlayerLevel
+                kappaRequired
+                lightkeeperRequired
+                objectives {
+                  id
+                  type
+                  description
+                  optional
+                  __typename
+                  ... on TaskObjectiveItem {
+                    count
+                    foundInRaid
+                  }
+                  ... on TaskObjectiveShoot {
+                    count
+                  }
+                  ... on TaskObjectiveQuestItem {
+                    count
+                  }
+                  ... on TaskObjectiveExtract {
+                    count
+                  }
+                  ... on TaskObjectivePlayerLevel {
+                    count: playerLevel
+                  }
+                  ... on TaskObjectiveSkill {
+                    skillLevel {
+                      level
                     }
-                    taskImageLink
-                    minPlayerLevel
-                    kappaRequired
-                    lightkeeperRequired
-                    objectives {
-                      id
-                      type
-                      description
-                      optional
-                      __typename
-                      ... on TaskObjectiveItem {
-                        count
-                        foundInRaid
-                      }
-                      ... on TaskObjectiveShoot {
-                        count
-                      }
-                      ... on TaskObjectiveQuestItem {
-                        count
-                      }
-                      ... on TaskObjectiveExtract {
-                        count
-                      }
-                      ... on TaskObjectivePlayerLevel {
-                        count: playerLevel
-                      }
-                      ... on TaskObjectiveSkill {
-                        skillLevel {
-                          level
-                        }
-                      }
-                      ... on TaskObjectiveTraderLevel {
-                        count: level
-                      }
-                      ... on TaskObjectiveTraderStanding {
-                        count: value
-                      }
-                      ... on TaskObjectiveExperience {
-                        count
-                      }
-                      ... on TaskObjectiveUseItem {
-                        count
+                  }
+                  ... on TaskObjectiveTraderLevel {
+                    count: level
+                  }
+                  ... on TaskObjectiveTraderStanding {
+                    count: value
+                  }
+                  ... on TaskObjectiveExperience {
+                    count
+                  }
+                  ... on TaskObjectiveUseItem {
+                    count
+                  }
+                }
+              }
+            }
+            """;
+
+    private static final String QUERY_ITEMS = """
+            {
+              items(name: "%s", lang: zh) {
+                id
+                name
+                shortName
+                basePrice
+                updated
+                width
+                height
+                iconLink
+                gridImageLink
+                avg24hPrice
+                lastLowPrice
+                low24hPrice
+                sellFor {
+                  priceRUB
+                  vendor {
+                    __typename
+                  }
+                }
+                usedInTasks {
+                  id
+                  name
+                  objectives {
+                    type
+                    ... on TaskObjectiveItem {
+                      count
+                      foundInRaid
+                      item {
+                        id
+                        name
                       }
                     }
                   }
                 }
-                """;
+                craftsUsing {
+                  station{
+                    name
+                  }
+                  level
+                  requiredItems {
+                    item {
+                      id
+                      name
+                    }
+                    count
+                  }
+                }
+              }
+            }
+            """;
+    private static final String QUERY_ITEM_ID = """
+            {
+              item(id: "%s", lang: zh) {
+                id
+                name
+                shortName
+                basePrice
+                updated
+                width
+                height
+                iconLink
+                gridImageLink
+                avg24hPrice
+                lastLowPrice
+                low24hPrice
+                sellFor {
+                  priceRUB
+                  vendor {
+                    __typename
+                  }
+                }
+                usedInTasks {
+                  id
+                  name
+                  objectives {
+                    type
+                    ... on TaskObjectiveItem {
+                      count
+                      foundInRaid
+                      item {
+                        id
+                        name
+                      }
+                    }
+                  }
+                }
+                craftsUsing {
+                  station{
+                    name
+                  }
+                  level
+                  requiredItems {
+                    item {
+                      id
+                      name
+                    }
+                    count
+                  }
+                }
+              }
+            }
+            """;
+
+    private static String getQueryRes(String query) {
         HashMap<String, String> queryMap = new HashMap<>();
         queryMap.put("query", query);
-        return queryMap;
+        String url = "https://api.tarkov.dev/graphql";
+        HttpClient httpClient = HttpUtil.getHttpClient();
+        HttpRequest httpRequest = HttpRequest.newBuilder()
+                .uri(URI.create(url))
+                .header("Content-Type", "application/json")
+                .header("Accept", "application/json")
+                .POST(HttpRequest.BodyPublishers.ofString(JSON.toJSONString(queryMap)))
+                .build();
+        return HttpUtil.request(httpClient, url, httpRequest);
     }
 
     @AnyMessageHandler()
@@ -303,6 +412,138 @@ public class TkfServerPlugin {
                 ***
                 \{stringBuilder.toString()}""";
         return mdText;
+    }
+
+
+    @AnyMessageHandler()
+    @MessageHandlerFilter(cmd = Regex.TKF_MARKET_SEARCH, at = AtEnum.NEED)
+    public void tkfMarketSearchAt(Bot bot, AnyMessageEvent event, Matcher matcher) {
+        log.info("groupId：{} qq：{} 请求 {}", event.getGroupId(), event.getUserId(), Regex.TKF_MARKET_SEARCH);
+        ExceptionHandler.with(bot, event, () -> {
+            String text = matcher.group("text").trim();
+            if (text.isEmpty()) {
+                return "";
+            }
+            return CacheUtil.getOrPut(text, 3, TimeUnit.MINUTES, () -> {
+                JSONArray jsonArray;
+                if (text.length() == 24) {
+                    String formatted = QUERY_ITEM_ID.formatted(text);
+                    String request = getQueryRes(formatted);
+                    JSONObject jsonObject = JSON.parseObject(request).getJSONObject("data").getJSONObject("item");
+                    if (jsonObject == null) {
+                        return STR."查询失败或物品不存在：\{text}";
+                    }
+                    jsonArray = JSONArray.of(jsonObject);
+                } else {
+                    String formatted = QUERY_ITEMS.formatted(text);
+                    String request = getQueryRes(formatted);
+                    jsonArray = JSON.parseObject(request).getJSONObject("data").getJSONArray("items");
+                    if (jsonArray.isEmpty()) {
+                        return STR."查询失败或物品不存在：\{text}";
+                    }
+                }
+
+                JSONObject item = jsonArray.getJSONObject(0);
+
+                Integer avg24hPrice = item.getInteger("avg24hPrice");
+                Integer lastLowPrice = item.getInteger("lastLowPrice");
+                String updated = item.getString("updated");
+                String id = item.getString("id");
+                OffsetDateTime offsetDateTime = OffsetDateTime.parse(updated, DateTimeFormatter.ISO_OFFSET_DATE_TIME);
+                LocalDateTime localDateTime = offsetDateTime.toLocalDateTime();
+                String formatTime = LocalDateTimeUtil.formatNormal(localDateTime);
+                JSONArray sellFor = item.getJSONArray("sellFor");
+                Integer maxPrice = 0;
+                for (Object o : sellFor) {
+                    JSONObject sFor = (JSONObject) o;
+                    String typename = sFor.getJSONObject("vendor").getString("__typename");
+                    Integer priceRUB = sFor.getInteger("priceRUB");
+                    if ("TraderOffer".equalsIgnoreCase(typename) && priceRUB > maxPrice) {
+                        maxPrice = priceRUB;
+                    }
+                }
+                JSONArray usedInTasks = item.getJSONArray("usedInTasks");
+                StringBuilder taskSB = new StringBuilder();
+                for (Object usedInTask : usedInTasks) {
+                    JSONObject task = (JSONObject) usedInTask;
+                    Integer needNum = 0;
+                    Boolean foundInRaid = false;
+                    task.getString("name");
+                    JSONArray objectives = task.getJSONArray("objectives");
+                    for (Object objective : objectives) {
+                        JSONObject obj = (JSONObject) objective;
+                        if (obj.getString("type").equalsIgnoreCase("giveItem")) {
+                            JSONArray objItems = obj.getJSONArray("item");
+                            for (Object objItem : objItems) {
+                                JSONObject objItemJ = (JSONObject) objItem;
+                                if (objItemJ.getString("id").equalsIgnoreCase(id)) {
+                                    needNum = obj.getInteger("count");
+                                    foundInRaid = obj.getBoolean("foundInRaid");
+                                    break;
+                                }
+                            }
+                        }
+                    }
+
+                    taskSB.append(STR."> \{task.getString("name")}( \{needNum}\{foundInRaid ? "√" : ""} )\n");
+                }
+                if (taskSB.isEmpty()) {
+                    taskSB.append("> 无");
+                }
+
+                StringBuilder craftSB = new StringBuilder();
+                JSONArray craftsUses = item.getJSONArray("craftsUsing");
+                for (Object craftsUs : craftsUses) {
+                    JSONObject craft = (JSONObject) craftsUs;
+                    String name = craft.getJSONObject("station").getString("name");
+                    String level = craft.getString("level");
+                    Integer count = 0;
+                    JSONArray requiredItems = craft.getJSONArray("requiredItems");
+                    for (Object requiredItem : requiredItems) {
+                        JSONObject requiredItemJ = (JSONObject) requiredItem;
+                        if (requiredItemJ.getJSONObject("item").getString("id").equalsIgnoreCase(id)) {
+                            count = requiredItemJ.getInteger("count");
+                            break;
+                        }
+                    }
+                    craftSB.append(STR."> \{name} lv.\{level} ( \{count} )\n");
+                }
+                if (craftSB.isEmpty()) {
+                    craftSB.append("> 无");
+                }
+
+                String mdText = STR."""
+                ![icon #35px #35px](\{item.getString("iconLink")}): \{item.getString("name")}
+                ***
+                24h均价: \{avg24hPrice == 0 ? STR."\{avg24hPrice} ₽" : "跳蚤禁售"}
+                跳蚤现价: \{avg24hPrice == 0 ? STR."\{lastLowPrice} ₽" : "跳蚤禁售"}
+                商人价格: \{maxPrice} ₽
+                任务需求:
+
+                \{taskSB.toString()}
+                藏身处需求:
+
+                \{craftSB.toString()}
+                ***
+                \{formatTime}
+                """;
+
+                Keyboard keyboard = Keyboard.Builder();
+                if (jsonArray.size() > 1) {
+                    for (int i = 1; i < jsonArray.size(); i++) {
+                        String idOther = jsonArray.getJSONObject(i).getString("id");
+                        String nameOther = jsonArray.getJSONObject(i).getString("name");
+                        keyboard.addRow().addButton(nameOther, STR."跳蚤 \{idOther}", true, List.of(event.getUserId()));
+                        if (i >= 5) {
+                            break;
+                        }
+                    }
+                } else {
+                    keyboard.addRow().addButton("我也要查", "跳蚤 ");
+                }
+                return BotUtil.getMarkdownMsg(bot, event, mdText, keyboard);
+            });
+        });
     }
 
 }
