25c6e2b73d0f6d83a7da6b9379feb3352f132415
KuroNeko <1419229777@qq.com>
2024-10-25 16:10:39 +0800

    update: 增加UA

--- src/main/java/com/kuroneko/cqbot/utils/PuppeteerUtil.java
+++ src/main/java/com/kuroneko/cqbot/utils/PuppeteerUtil.java
@@ -67,7 +67,8 @@ public class PuppeteerUtil {
 //                    "--single-process",
                     "--disable-blink-features=AutomationControlled",
                     "--disable-extensions",
-                    "--no-zygote"
+                    "--no-zygote",
+                    "--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36"
             );
             LaunchOptions options = new LaunchOptionsBuilder().withArgs(list).withHeadless(true).build();
             browser = Puppeteer.launch(options);
