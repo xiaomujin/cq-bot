c2fae288548ff72c20e7d3c8d58be0d1d88bdc4a
KuroNeko <1419229777@qq.com>
2025-02-12 17:22:22 +0800

    :recycle: 替换过时方法

--- src/main/java/com/kuroneko/cqbot/config/TemplateConfig.java
+++ src/main/java/com/kuroneko/cqbot/config/TemplateConfig.java
@@ -3,6 +3,7 @@ package com.kuroneko.cqbot.config;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.client.ClientHttpRequestFactory;
 import org.springframework.http.client.ClientHttpResponse;
 import org.springframework.http.converter.HttpMessageConverter;
@@ -11,6 +12,7 @@ import org.springframework.web.client.ResponseErrorHandler;
 import org.springframework.web.client.RestTemplate;
 
 import java.io.IOException;
+import java.net.URI;
 import java.util.List;
 
 @Configuration
@@ -45,7 +47,7 @@ public class TemplateConfig {
             }
 
             @Override
-            public void handleError(ClientHttpResponse response) throws IOException {
+            public void handleError(URI url, HttpMethod method, ClientHttpResponse response) throws IOException {
 
             }
         });
